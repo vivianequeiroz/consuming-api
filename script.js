e6ee40740c70fcd75532b2b3095773542617d07e
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-08-23 15:44:24 -0300

    chore: cors lib added to display api content at the browser

--- script.js
+++ script.js
@@ -3,9 +3,25 @@
 async function getContent() {
   try {
     const response = await fetch("http://localhost:4567/");
+    console.log(response);
+
+    const data = await response.json();
+    showData(data);
+
   } catch (error) {
     console.log(error);
   }
 }
 
 getContent();
+
+function showData(users) {
+
+  let output = '';
+
+  for (let user of users) {
+    output += `<li>${user.name}</li>`;
+  }
+
+  document.querySelector('p').innerHTML = output;
+}
